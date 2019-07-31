4d4aa7bbd7aa3bd1845f418726470d8784064dbe
adrianos23 <53499693+adrianos23@users.noreply.github.com>
2019-07-31 09:30:55 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,29 @@
 <html>
-<head><title>Hello</title>
-</head>
+<head>
 <body>
-Hello World!
+<strong>
+</head>
+<a href="H:\prelim">Visit my page</a>
+<h1><b>My Profile</b></h1>
+<img src="adrianos.jpg" width="200" height="260">
+<p>My name is Adrian Paolo, I love many things and one of them is computers</p>
+<ol><h3>Favorite Movies</h3>
+<li><i>Interstellar</i></li>
+<li><u>John Wick</u></li>
+<li><em>Logan</em></li>
+</ol>
+<ul><h3>Hobbies</h3>
+<li><i>Basketball</i></li>
+<li><u>Playing Guitar</u></li>
+<li><em>Gym</em></li>
+</ul>
+<table style width="100">
+<tr>
+	<th>Height: 5</th>
+	<th>Weight: 56lbs</th>
+</tr>
+</table>
+
 </html>
 </body>
+</strong>
