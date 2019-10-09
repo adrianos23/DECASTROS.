a7577a80bae3c3e41e7caf0813ee6bf7ef6d6ef8
adrianos23 <53499693+adrianos23@users.noreply.github.com>
2019-10-09 09:49:27 +0800

    Rename index.php to index1.php

---
+++ index1.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html>
+<head><title>Adrianos Wallpaper</title></head>
+<body>
+<style>
+body {
+  background-color: lightblue;
+}
+</style>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+.dropbtn {
+  background-color: blue;
+  color: white;
+  padding: 16px;
+  font-size: 16px;
+  border: none;
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: blue;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+.dropdown-content a:hover {background-color: #ddd;}
+
+.dropdown:hover .dropdown-content {display: block;}
+
+.dropdown:hover .dropbtn {background-color: #3e8e41;}
+img {
+  border-radius: 8px;
+}
+</style>
+</head>
+<body>
+<div class="dropdown">
+  <button class="dropbtn">Choose Category</button>
+  <div class="dropdown-content">
+    <a href="#">PC</a>
+    <a href="#">Android</a>
+    <a href="#">IOS</a>
+  </div>
+</div>
+<h1>DeCastroA Free HD Wallpaper</h1>
+	<p>Download Free Wallpapers with any platforms</p>
+	<img src="view.jpg" align="right" alt="Mountain View" width="500" height="350">
+	<h2>List of Photos</h2>
+	<a href="#">Photo 1</a>
+	<br>
+	<a href="#">Photo 2</a>
+	<br>
+	<a href="#">Photo 3</a>
+	<br/>
+	<br/>
+	<br/>
+	</br>
+<h2>Wallpapers</h2>
+	<img src="30c.jpg" alt="Homer" width="250" height="295">
+	<img src="loga.jpg" alt="Wolverine" width="250" height="240">
+</body>
+</html>
